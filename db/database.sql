e5c0ce642d135c476aaa247ff3a683b2762def5d
Jose Luis Sanchez <jwich_07@hotmail.com>
2020-05-22 19:10:35 -0500

    first commit

--- db/database.sql
+++ db/database.sql
@@ -12,14 +12,15 @@ CREATE TABLE users (
   photo VARCHAR(45) DEFAULT NULL,
   age INT(10) DEFAULT NULL,
   status INT(10) DEFAULT 1,
+  token VARCHAR(45) DEFAULT NULL,
   PRIMARY KEY(iduser)
 );
 
 DESCRIBE users;
 
 INSERT INTO users values 
-  (1, 'Jose Luis Reyes Sanchez', 'josc.rar@gmail.com', '123456', '5533012377', 'Toluca, Estado de México', '', 23, 1),
-  (2, 'Alejandro Perez', 'email@domain.com', '123456', '98052345345', 'Toluca, Estado de México', '', 23, 1);
+  (1, 'Jose Luis Reyes Sanchez', 'josc.rar@gmail.com', '123456', '5533012377', 'Toluca, Estado de México', '', 23, 1, 'TYSMCM290723NKSAVSA'),
+  (2, 'Alejandro Perez', 'email@domain.com', '123456', '98052345345', 'Toluca, Estado de México', '', 23, 1, 'IOUFBALSBCL9038742JKBSACJSA');
 
 
 SELECT * FROM users;
